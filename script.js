df876bfa9f48cacfee7595e9b4aa1d825ad627b2
rozitasaaki <rozita.saaki62@gmail.com>
2025-01-02 00:28:06 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([29.7063, 52.1147]).addTo(map)
     .bindPopup('Shiraz, Iran')
     .openPopup();
+	// Load GeoJSON data and add it to the map
+fetch('mydata.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data).addTo(map);
+    })
+    .catch(error => console.error('Error loading GeoJSON:', error));
+
